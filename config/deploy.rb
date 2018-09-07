15f31fc3ebe0b8fc7d32f0c892f4b869d9bba46b
misha-pelykh <michaelpelykh@gmail.com>
2018-09-07 13:02:59 +0300

    change deployment configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,13 +25,16 @@ set :deploy_to, "/home/#{user}/apps/milestep"
 
 set :rbenv_type, :user # or :system, depends on your rbenv setup
 set :rbenv_ruby, '2.3.1'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
 
-# Default value for :linked_files is []
+# Default value for :linked_f iles is []
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/master.key')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push(
-  'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads'
+  'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 )
 
 # Default value for default_env is {}
