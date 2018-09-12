96c21711fb9a1189968e6f2675d9fc93438924fb
Djnt <dijientt@gmail.com>
2018-09-12 13:50:22 +0300

    tabs

--- app/assets/javascripts/header/burger-dropdown.js
+++ app/assets/javascripts/header/burger-dropdown.js
@@ -1,6 +1,6 @@
 function burgerAction(){
-    $('.burger-hidable').map((index, element) => {
-        var style = $(element).css('display') == 'none' ? 'block' : 'none'
-        $(element).css('display', style)
-    })
+	$('.burger-hidable').map((index, element) => {
+		var style = $(element).css('display') == 'none' ? 'block' : 'none'
+		$(element).css('display', style)
+	})
 }
