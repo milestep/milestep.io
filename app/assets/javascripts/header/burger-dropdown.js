e09c3b03007d20b2e8b26624c2a0b98c5601470f
Djnt <dijientt@gmail.com>
2018-09-13 11:55:54 +0300

    header pixel perfect fix && burger button slide left

--- app/assets/javascripts/header/burger-dropdown.js
+++ app/assets/javascripts/header/burger-dropdown.js
@@ -1,4 +1,7 @@
 function burgerAction(){
+  var burgerPos = ($('.burger-button').css('left')).slice(0, -2)/ document.documentElement.clientWidth 
+  burgerPos = burgerPos < 0.77 ? '77.3vw' : '76.3vw'  
+  $('.burger-button').css('left', burgerPos)
   $('.burger-hidable').map((index, element) => {
     var style = $(element).css('display') == 'none' ? 'block' : 'none'
     $(element).css('display', style)
