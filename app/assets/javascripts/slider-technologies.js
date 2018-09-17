903d13b99ed069c84746c9d1e928286189d3998d
Djnt <dijientt@gmail.com>
2018-09-17 20:04:09 +0300

    scroll w/out reload

--- app/assets/javascripts/slider-technologies.js
+++ app/assets/javascripts/slider-technologies.js
@@ -1,23 +1,10 @@
-let left = 0;
-
-function sliderLeft() {
-  var polosa = document.getElementById('polosa');
-
-  left = left - 300;
-
-  if (left < -2400)
-    left = 0;
-
-  polosa.style.left = left + 'px';
-}
-
-function sliderRight() {
-  var polosa = document.getElementById('polosa');
-
-  left = left + 300;
-
-  if (left > 0)
-    left = -2400;
-
-  polosa.style.left = left  + 'px';
-}
+$(document).ready(function() {
+  $('.slider-nav').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: $('.arrow-prev-js'),
+    nextArrow: $('.arrow-next-js')
+  });
+});
