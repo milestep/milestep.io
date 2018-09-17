a74b39e4142459e95579532c14316bd453888570
Djnt <dijientt@gmail.com>
2018-09-17 21:50:56 +0300

    hash_nav disable

--- app/assets/javascripts/zoom.js
+++ app/assets/javascripts/zoom.js
@@ -1,4 +1,12 @@
 jQuery(document).ready(function() {
+  applyZoom()
+})
+
+function applyZoom() {
   document.querySelector('body').style.setProperty('--origin-width', window.devicePixelRatio * $(document).width())
   document.querySelector('body').style.setProperty('--font-size-offset', ((window.devicePixelRatio * $(document).width()) / 1920))
-})
+}
+
+$(window).bind('hashchange', function (e) { 
+  location.reload(true)
+});
