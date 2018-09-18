c3b56989b37f3f644d867e0c5f045eb9d34c2094
Djnt <dijientt@gmail.com>
2018-09-18 09:45:33 +0300

    fixes

--- app/assets/javascripts/zoom.js
+++ app/assets/javascripts/zoom.js
@@ -1,12 +1,13 @@
 jQuery(document).ready(function() {
   applyZoom()
+  $('.main-container').css('display', 'block')
 })
 
 function applyZoom() {
-  document.querySelector('body').style.setProperty('--origin-width', window.devicePixelRatio * $(document).width())
-  document.querySelector('body').style.setProperty('--font-size-offset', ((window.devicePixelRatio * $(document).width()) / 1920))
+  document.querySelector('body').style.setProperty('--origin-width', window.devicePixelRatio * window.screen.width)
+  document.querySelector('body').style.setProperty('--font-size-offset', ((window.devicePixelRatio * window.screen.width) / 1935))
 }
 
 $(window).bind('hashchange', function (e) { 
-  location.reload(true)
+  location.reload(false)
 });
