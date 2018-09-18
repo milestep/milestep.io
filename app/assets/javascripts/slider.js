93f14e3516a814058ffdb716d92420192d1d0a79
JARVIS-VOVA <jarvis.vova@gmail.com>
2018-09-18 10:48:43 +0300

    Add slider-info-number-page-js to team

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -7,16 +7,21 @@ $(document).ready(function() {
     prevArrow: $('.arrow-prev-js'),
     nextArrow: $('.arrow-next-js')
   });
-});
 
-$(document).ready(function() {
   $('.slider-team-slick-js').slick({
     rows: 2,
-    slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesPerRow: 3,
     autoplay: true,
     dots: true,
     autoplaySpeed: 2000,
-    arrows: false,
+    arrows: false
+  });
+
+  var $status = $('.slider-info-number-page-js');
+  var $slickElement = $('.slider-team-slick-js');
+
+  $slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+    var i = (currentSlide ? currentSlide : 0) + 1;
+    $status.text('0' + i + ' / ' + '0' + slick.slideCount);
   });
 });
