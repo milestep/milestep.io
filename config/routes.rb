ee7016ce21bac55464a1b574e8d17c8c5f393113
Volodymyr Bystranivskyi <vova.b@milestep.io>
2018-09-21 14:42:24 +0300

    Change blog route to use subdomain

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  
+  constraints subdomain: "blog" do
+    get '/', to: 'blog#index'
+    resources :posts, only: [:show]
+  end
+
   root 'welcome#index'
 
   get 'welcome/index', to: 'welcome#index'
   post :contact, to: 'requests#create', as: :contact
-
-  get '/blog', to: 'blog#index'
-  resources :posts, only: [:show], path: "/blog"
 end
