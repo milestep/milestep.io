b4c9f4f208aa176f5ffcfa98882f907f0160003c
Volodymyr Bystranivskyi <vova.b@milestep.io>
2018-09-24 16:27:09 +0300

    Add main_post_image to show at post-edit in ActiveAdmin

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Post do
   menu parent: 'Blog'
-  permit_params :title, :body, :author_id, :image_link
+  permit_params :title, :body, :author_id, :image_link, :main_post_image
 
   index do
     selectable_column
@@ -9,6 +9,7 @@ ActiveAdmin.register Post do
     column :body
     column :author_id
     column :image_link
+    column :main_post_image
     actions
   end
 
@@ -18,6 +19,7 @@ ActiveAdmin.register Post do
       f.input :body, as: :ckeditor
       f.input :author 
       f.input :image_link, hint: f.post.image_link? ? image_tag(post.image_link, height: '300') : content_tag(:span, "Upload JPG/PNG/GIF image")
+      f.input :main_post_image, hint: f.post.main_post_image? ? image_tag(post.main_post_image, height: '300') : content_tag(:span, "Upload JPG/PNG/GIF image")
     end
     f.actions
   end
