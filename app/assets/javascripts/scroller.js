d5a737973aeae08966c4bbe7a0cf88fb680720c2
Djnt <dijientt@gmail.com>
2018-09-24 20:18:20 +0300

    missing queries fix

--- app/assets/javascripts/scroller.js
+++ app/assets/javascripts/scroller.js
@@ -19,3 +19,19 @@ jQuery.event.special.touchstart = passiveEvent;
 jQuery.event.special.touchmove = passiveEvent;
 jQuery.event.special.mousewheel = passiveEvent;
 jQuery.event.special.wheel = passiveEvent;
+
+$(document).on('turbolinks:load', function() {
+  var w = $('.main-container').width()
+  if(w != screen.width && screen.width > 800) {
+    var query = `
+      @media screen and (min-width: ${window.innerWidth}px) {
+        :root {
+          --origin-width: ${window.innerWidth};
+          --font-size-offset: ${window.innerWidth/1920.0};
+          --statc-font-size-offset: ${window.innerWidth/1920.0};
+        }
+      }
+    `
+    $( `<style>${query}</style>` ).appendTo( "head" )
+  }
+});
