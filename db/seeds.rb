c7770373444ee5b6897b6862c7dce46caff00d98
Volodymyr Bystranivskyi <vova.b@milestep.io>
2018-09-25 16:29:59 +0300

    Uncomment seed for proper display of pagination

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,13 +16,12 @@ author = Author.create!(
   )
 end
 
-# 32.times do
-#   author.posts.create!(
-#     title: 'Dedicated Team - is it an Art or a Science?', 
-#     body: 'Our world is full of the eternal questions. What do we live for? Is there a life beyond Earth? Whom does dedicated team model work for?...',
-#     image_link: 'blog/post_img_1',
-#     main_post_image: 'blog/post/main_post_image'
-#     )
-# end
+32.times do
+  author.posts.create!(
+    title: 'Dedicated Team - is it an Art or a Science?', 
+    body: 'Our world is full of the eternal questions. What do we live for? Is there a life beyond Earth? Whom does dedicated team model work for?...',
+      thumbnail_image: File.open(File.join(Rails.root.join('app', 'assets', 'images', 'blog', 'post_img_1.jpg'))),
+      main_image: File.open(File.join(Rails.root.join('app', 'assets', 'images', 'blog', 'post', 'main_post_image.jpg')))
+end
 
 puts 'Successfully completed!!!'
