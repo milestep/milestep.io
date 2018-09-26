8280d54a09b117f4d421ba707762ec09b72c8694
djnt <dijientt@gmail.com>
2018-09-26 17:24:15 +0300

    slick init fix

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -8,6 +8,14 @@ $(document).ready(function() {
     nextArrow: $('.arrow-next-js')
   });
 
+  var $status = $('.slider-info-number-page-js');
+  var $slickElement = $('.slider-team-slick-js');
+
+  $slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+    var i = (currentSlide ? currentSlide : 0) + 1;
+    $status.text('0' + i + ' / ' + '0' + slick.slideCount);
+  });
+
   $('.slider-team-slick-js').slick({
     rows: 2,
     slidesPerRow: 3,
@@ -15,13 +23,5 @@ $(document).ready(function() {
     dots: true,
     autoplaySpeed: 2000,
     arrows: false
-  });
-
-  var $status = $('.slider-info-number-page-js');
-  var $slickElement = $('.slider-team-slick-js');
-
-  $slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
-    var i = (currentSlide ? currentSlide : 0) + 1;
-    $status.text('0' + i + ' / ' + '0' + slick.slideCount);
   });
 });
