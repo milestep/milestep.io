422c83dbc0c979f0a1cbab875d8374023d5bff52
misha-pelykh <michaelpelykh@gmail.com>
2018-10-01 14:02:49 +0300

    fix application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,4 @@
+
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -12,9 +13,7 @@
 //
 //= require rails-ujs
 //= require activestorage
-//= require jquery
-//= require turbolinks
-//= require popper
+//= require jquery3
 //= require jquery.slick
 //= require ckeditor/init
 //= require_tree .
