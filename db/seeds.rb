23986bda7d70cb39daf7d0bac70c844de84345b6
Volodymyr Bystranivskyi <vova.b@milestep.io>
2018-10-01 12:41:27 +0300

    Add order to team_members and portfolio_items tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,6 +22,7 @@ end
     body: 'Our world is full of the eternal questions. What do we live for? Is there a life beyond Earth? Whom does dedicated team model work for?...',
       thumbnail_image: File.open(File.join(Rails.root.join('app', 'assets', 'images', 'blog', 'post_img_1.jpg'))),
       main_image: File.open(File.join(Rails.root.join('app', 'assets', 'images', 'blog', 'post', 'main_post_image.jpg')))
+  )
 end
 
 puts 'Successfully completed!!!'
