01bbbf62abb13f909acc59f9d5d09fdcdd851342
misha-pelykh <michaelpelykh@gmail.com>
2018-10-02 21:01:53 +0300

    create scrolling and background

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def scrolling_button(params=nil, &block)
+    options = {
+      offset_top: '50%',
+      background: 'transparent'
+    }.merge(params || {})
+
+    wrapper_styles = "background: #{options[:background]}; top: #{options[:offset_top]}"
+
+    content_tag :div, class: 'scroller-btn-container' do
+      content_tag :div, class: 'scrolling-button', style: wrapper_styles do
+        content_tag :div, class: 'text-wrapper' do
+          yield block
+        end
+      end
+    end
+  end
 end
