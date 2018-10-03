86fb070b85e345cfdc77449614cfc17cc8b35927
Volodymyr Bystranivskyi <vova.b@milestep.io>
2018-10-03 11:20:29 +0300

    Get rid of ;unnecessary variables

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,6 +1,5 @@
 module PostsHelper
   def get_html_text_for_meta_tag(raw_html)
-    html_doc = Nokogiri::HTML(raw_html)
-    paragraph = html_doc.css('p').first.text
+    Nokogiri::HTML(raw_html).css('p').first.text
   end
 end
