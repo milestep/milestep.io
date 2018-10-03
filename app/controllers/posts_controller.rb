83784150572337058616b686a5a6586644b1aa5c
Volodymyr Bystranivskyi <vova.b@milestep.io>
2018-10-03 14:24:22 +0300

    Refactor meta tags generation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,17 +12,7 @@ class PostsController < ApplicationController
   end
 
   def set_meta_tags
-    meta_tags = { 
-      twitter_description: helpers.cut_text(ActionController::Base.helpers.strip_tags(helpers.get_html_text_for_meta_tag(@post.body)), 300),
-      twitter_title: @post.title,
-      twitter_image: ActionController::Base.helpers.image_url(@post.main_image),
-      og_title: @post.title,
-      og_type: "article",
-      og_description: helpers.cut_text(ActionController::Base.helpers.strip_tags(helpers.get_html_text_for_meta_tag(@post.body)), 300),
-      og_url: post_url(@post),
-      og_site_name: "MileStep | Web &amp; Mobile App Development Company",
-      og_image: ActionController::Base.helpers.image_url(@post.main_image)
-    }
+    meta_tags = helpers.generate_meta_tag_from_post(@post)
     @meta_tags = merge_into_default_meta_tags(meta_tags)
   end
 end
