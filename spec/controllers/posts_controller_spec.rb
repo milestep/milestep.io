d6185611023f788c857b1fea12eebd5fefeb49c7
Volodymyr Bystranivskyi <vova.b@milestep.io>
2018-10-05 13:57:00 +0300

    Add factories for author and post models

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,9 +2,14 @@ require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
 
+  before(:each) do
+    author = create(:author)
+    @post = create(:post)
+  end
+
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      get :show, params: {  id: @post }
       expect(response).to have_http_status(:success)
     end
   end
