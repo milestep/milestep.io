6b048ea4e06d06d49530848f39fa7a9e341fad4d
VitalikYaroviy <vitalya.yaroviy97@gmail.com>
2018-12-24 10:23:26 +0200

    update design

--- app/assets/javascripts/header/burger-dropdown.js
+++ app/assets/javascripts/header/burger-dropdown.js
@@ -1,9 +1,14 @@
 function burgerAction(){
   var burgerPos = ($('.burger-button').css('left')).slice(0, -2)/ document.documentElement.clientWidth 
-  burgerPos = burgerPos < 0.77 ? '77.3vw' : '76.3vw'  
-  $('.burger-button').css('left', burgerPos)
+  burgerPos = burgerPos < 0.77 ? '76.3vw' : '74.3vw'
+  //$('.burger-button').css('left', burgerPos)
   $('.burger-hidable').map((index, element) => {
     var style = $(element).css('display') == 'none' ? 'block' : 'none'
     $(element).css('display', style)
+    if (style === 'block') {
+      $('#burger')[0].src = 'assets/header/close.svg'
+    } else {
+      $('#burger')[0].src = 'assets/header/burger.png'
+    }
   })
 }
