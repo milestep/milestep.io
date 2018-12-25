41ad6cb6f38df3620daf1cf0e1c04bdacca9150c
Eugene Korpan <eugene@active-bridge.com>
2018-12-25 12:31:23 +0200

    Update request email recipient

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'platform.naprok.com' }
+  config.action_mailer.default_url_options = { host: 'milestep.io' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
@@ -99,7 +99,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.mailgun.org',
     port: 587,
-    domain: 'mg.naprok.com',
+    domain: 'milestep.io',
     authentication: 'plain',
     enable_starttls_auto: true,
     user_name: ENV['MAILGUN_USERNAME'],
