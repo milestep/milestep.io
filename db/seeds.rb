82d9ec59afd875d84943d0159ba3dc06ab7c3638
VitalikYaroviy <vitalya.yaroviy97@gmail.com>
2019-01-14 15:48:29 +0200

    added design blog for tablet and mobile

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,13 +28,13 @@ if Rails.env.development?
 
       <h3 dir="ltr" style="text-align: justify;"><span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;"><strong>1. Increasing monthly payments</strong></span></span></h3>
 
-      <p style="text-align: justify; margin-left: 40px;"><span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;">One of the main advantages of working with an outsourcing company is retrenchment of costs. To be sure that you will stay on budget you should:</span></span></p>
+      <p style="text-align: justify;"><span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;">One of the main advantages of working with an outsourcing company is retrenchment of costs. To be sure that you will stay on budget you should:</span></span></p>
 
       <ul dir="ltr">
-        <li style="text-align: justify; margin-left: 40px;">◦&nbsp;<span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;">Have a clear vision of what you want to develop with the help of outsourcing company;</span></span></li>
-        <li style="text-align: justify; margin-left: 40px;">◦&nbsp;<span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;">Have the final goal of that creation;</span></span></li>
-        <li style="text-align: justify; margin-left: 40px;">◦&nbsp;<span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;">Define functions it must perform;</span></span></li>
-        <li style="text-align: justify; margin-left: 40px;">◦&nbsp;<span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;">Estimate the time duration of cooperation with offshore members.</span></span></li>
+        <li style="text-align: justify;">◦&nbsp;<span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;">Have a clear vision of what you want to develop with the help of outsourcing company;</span></span></li>
+        <li style="text-align: justify;">◦&nbsp;<span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;">Have the final goal of that creation;</span></span></li>
+        <li style="text-align: justify;">◦&nbsp;<span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;">Define functions it must perform;</span></span></li>
+        <li style="text-align: justify;">◦&nbsp;<span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;">Estimate the time duration of cooperation with offshore members.</span></span></li>
       </ul>
 
       <p dir="ltr" style="text-align: justify;"><span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;">When you know the scope of work and the time frames, you will reduce the number of potential candidates for collaboration. This might help you to assess the project budget better. </span></span></p>
@@ -43,7 +43,7 @@ if Rails.env.development?
 
       <p dir="ltr" style="text-align: justify;"><span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;">This necessary aspect must be considered carefully. To decrease this risk you need to follow this instruction: </span></span></p>
 
-      <ul dir="ltr" style="margin-left: 40px;">
+      <ul dir="ltr">
         <li style="text-align: justify;">◦&nbsp;<span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;">First&nbsp;off, you need to pick a few outsourcing companies that fit your requirements. Your friend&#39;s and partner&#39;s recommendations will ease this challenge; </span></span></li>
         <li style="text-align: justify;">◦&nbsp;<span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;">Pay attention to possible cultural differences between you and your future partner. The cooperation between members of the same type of culture will be more successful. American, European and Australian culture belong to the Western type. The culture of the Middle East countries, Central, South-East Asia and North Africa - is the Eastern type;</span></span></li>
         <li style="text-align: justify;">◦&nbsp;<span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;">Thereafter take a close look at the candidate&#39;s websites, check out their portfolio. The final product speaks better than any advertising. Download the application or visit the website and evaluate the quality indicators of performed work. Look among the completed projects similar to those you want to do. Pay attention to technologies the company operates with;</span></span></li>
@@ -56,7 +56,7 @@ if Rails.env.development?
 
       <p dir="ltr" style="text-align: justify;"><span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;">As you know, you will need to share your confidential information with an offshore dedicated team you will be working with. Do you want to protect your information? We are sure you do. Follow this list to decrease the security risks and ensure the safety of your data:</span></span></p>
 
-      <ul dir="ltr" style="margin-left: 40px;">
+      <ul dir="ltr">
         <li style="text-align: justify;">◦&nbsp;<span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;">Make sure your cooperation does not violate local laws and that it is compliant with international norms;</span></span></li>
         <li style="text-align: justify;">◦&nbsp;<span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;">Sign a non-disclosure agreement (NDA);</span></span></li>
         <li style="text-align: justify;">◦&nbsp;<span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;">Discuss the process of storing and encrypting the confidential data;</span></span></li>
@@ -67,7 +67,7 @@ if Rails.env.development?
 
       <p dir="ltr" style="text-align: justify;"><span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;">Good relationships between colleagues have great importance for communication. English language skills is the key to common understanding. To decrease this risk, you can:</span></span></p>
 
-      <ul dir="ltr" style="margin-left: 40px;">
+      <ul dir="ltr">
         <li style="text-align: justify;">◦&nbsp;<span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;">&nbsp;Check each eventual member of your dedicated team on language proficiency;</span></span></li>
         <li style="text-align: justify;">◦&nbsp;&nbsp;<span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;">Create an informal communication channel to form a team spirit;</span></span></li>
         <li style="text-align: justify;">◦&nbsp;<span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;">Give and take a feedback from your offshore partners, that allows you to keep a finger on the pulse of all working processes and opens the way to improve your product.</span></span></li>
@@ -79,7 +79,7 @@ if Rails.env.development?
 
       <p dir="ltr" style="text-align: justify;"><span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;">Different countries, different time zones - all these put imprint onto a workflow of a remote dedicated team. What can you do to get the teamwork process under control? Your actions are:</span></span></p>
 
-      <ul dir="ltr" style="margin-left: 40px;">
+      <ul dir="ltr">
         <li style="text-align: justify;">◦&nbsp;&nbsp;<span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;">To set up working hours when team members have to be available for communication;</span></span></li>
         <li style="text-align: justify;">◦&nbsp;&nbsp;<span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;">To assign tools for communication;</span></span></li>
         <li style="text-align: justify;">◦&nbsp;&nbsp;<span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;">To specify the number of video calls and progress reports in a week.</span></span></li>
@@ -89,7 +89,7 @@ if Rails.env.development?
 
       <p dir="ltr" style="text-align: justify;"><span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;">When the staff is outside the office, you can&#39;t evaluate performance in the process of development physically. If you want to assure yourself that everything is going in accordance to the plan, you ought to:</span></span></p>
 
-      <ul dir="ltr" style="margin-left: 40px;">
+      <ul dir="ltr">
         <li style="text-align: justify;">◦&nbsp;&nbsp;<span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;">Split the development process into pieces and determine a target of each of them. The effectiveness depends on a level of each goal implementation; </span></span></li>
         <li style="text-align: justify;">◦&nbsp;&nbsp;<span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;">Use project management and productivity tracking tools to operate your remote team of developers.</span></span></li>
       </ul>
@@ -98,7 +98,7 @@ if Rails.env.development?
 
       <p dir="ltr" style="text-align: justify;"><span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;">The cooperation with an offshore dedicated team has many advantages, let&rsquo;s overview them:</span></span></p>
 
-      <ul dir="ltr" style="margin-left: 40px;">
+      <ul dir="ltr">
         <li style="text-align: justify;">◦&nbsp;&nbsp;<span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;">Access to a workforce with the necessary experience and expertise; </span></span></li>
         <li style="text-align: justify;">◦&nbsp;&nbsp;<span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;">You don&#39;t need to hire people - your partners will do that job;</span></span></li>
         <li style="text-align: justify;">◦&nbsp;&nbsp;<span style="font-family:Trebuchet MS,Helvetica,sans-serif;"><span style="font-size:18px;">The rate flexibility allows you to save money on salaries;</span></span></li>
