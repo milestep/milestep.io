fb686ced3d9fbe14e78ac1bd0f9b0a3508d4e5c3
Sergey <sergeyripchanskiy@gmail.com>
2019-05-21 17:23:40 +0300

    attachment

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,11 +1,15 @@
 class RequestsController < ApplicationController
   def create
+    # pry
+    # respond_to :js
+    # pry
     Request.notify(attrs).deliver_later
   end
 
   private
 
   def attrs
-    @attrs ||= params[:contact].as_json
+    # @attrs ||= params[:contact].as_json
+    @attrs ||= params.permit!
   end
 end
--- app/mailers/request.rb
+++ app/mailers/request.rb
@@ -1,10 +1,21 @@
 class Request < ApplicationMailer
+  # def notify(attrs)
+  #   @body = attrs['message']
+  #   mail(
+  #     from: "#{attrs['email']}",
+  #     to: ENV["REQUEST_RECIPIENT"] || 'contact@milestep.io',
+  #     subject: "Contact Us"
+  #   )
+  # end
+
   def notify(attrs)
-    @body = attrs['message']
+    @body = attrs[:message]
+    attachment[attrs[:attachment].original_filename] = File.read(attrs[:attachment].path)
     mail(
-      from: "#{attrs['email']}",
-      to: ENV["REQUEST_RECIPIENT"] || 'contact@milestep.io',
-      subject: "Contact Us"
+        from: "#{attrs[:email]}",
+        to: ENV["REQUEST_RECIPIENT"] || 'contact@milestep.io',
+        subject: "Contact Us"
     )
+    #
   end
 end
