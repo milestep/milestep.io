fe4963a0dfcb839ec94d485f3fcb9b765f6d9122
AlexT <alex.t@milestep.io>
2019-05-23 11:22:21 +0300

    Added application_blog
    Signed-off-by: AlexT <alex.t@milestep.io>

--- app/assets/javascripts/application_mobile.js
+++ app/assets/javascripts/application_mobile.js
@@ -1,12 +1,8 @@
 //= require config
 //= require jquery3
-//= require jquery_ujs
 
 //= require jquery.featureCarousel
 //= require jquery.slickCarousel
-//= require circle-progress
 //= require paralax_mobile
 //= require send-message
 //= require send-mail
-
-//= require serviceworker-companion
