2a32095afaa95626b6e648b509fed2b6d01163cb
Sergey <sergeyripchanskiy@gmail.com>
2019-05-29 11:39:35 +0300

    fix migration technologies

--- db/migrate/20190524141226_add_technologies_to_portfolio_items.rb
+++ db/migrate/20190524141226_add_technologies_to_portfolio_items.rb
@@ -1,5 +1,5 @@
 class AddTechnologiesToPortfolioItems < ActiveRecord::Migration[5.2]
   def change
-    add_column :portfolio_items, :image_url, :string
+    add_column :portfolio_items, :technologies, :string
   end
 end
