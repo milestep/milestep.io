98cd19f4608c0b3ba749b853a0f6eae7ab63b724
Sergey <sergeyripchanskiy@gmail.com>
2019-06-07 10:42:21 +0300

    exclude some scripts, add of scroll for action when contact-us-button clicked

--- app/assets/javascripts/application_blog.js
+++ app/assets/javascripts/application_blog.js
@@ -1,14 +1,11 @@
 //= require preloader
 //= require jquery3
-//= require config
-//= require jquery.slick
 //= require activestorage
 //= require ckeditor/init
 
 //= require jquery.notiny
 //= require send-message
 //= require pictureChanger
-//= require scroller
 //= require pagination
 //= require blog
 
--- app/assets/javascripts/blog.js
+++ app/assets/javascripts/blog.js
@@ -1,6 +1,7 @@
 "use strict"
 jQuery(document).ready(function() {
   $('.blog-header .navigation-panel .contact-us-button').on('click', function () {
+    $(window).off('scroll');
     $([document.documentElement, document.body]).animate({
       scrollTop: $(document).height()
     }, 1000);
