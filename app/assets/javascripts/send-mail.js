a32ccf5468535d98649e9a04212a3d6c28b736b9
zzeenj <zzeenj@gmail.com>
2019-11-18 12:16:12 +0200

    fixed succes message after form delivered, changed validations for this one

--- app/assets/javascripts/send-mail.js
+++ app/assets/javascripts/send-mail.js
@@ -1,44 +1,65 @@
 function sendMail(e) {
-  e.preventDefault()
-  let data = validation($('.message-form')[0])
-  if (!data) return
+    e.preventDefault()
+    let data = validation($('.message-form')[0])
+    if (!data) return
 
-  fetch('/contact_us', { method: 'POST', body: new FormData(data) })
-    .then(function(response) {clearInputs(response.statusText)})
-    .catch(function(e) {notify('Error sending message', 'error')})
+    fetch('/contact_us', { method: 'POST', body: new FormData(data) })
+        .then(function(response) {clearInputs('Message was successfully delivered!')})
+        .catch(function(e) {notify('Unable to deliver your message!', 'error')})
 }
 
 function clearInputs(text) {
-  $.each($('.message-form input'), function(i, item) {
-    if (item.name == 'name' || item.name == 'email' || item.name == 'message') {
-      item.value = ''
-  }})
+    $.each($('.message-form input'), function(i, item) {
+        if (item.name == 'name' || item.name == 'email' || item.name == 'message') {
+            item.value = ''
+        }})
 
-  notify(text, 'success')
+    notify(text, 'success')
 }
 
 function notify(text, type) {
-  let params = { position: 'right-top', theme: type }
-  $.notiny({ text: text, ...params })
+    let params = { position: 'right-top', theme: type }
+    $.notiny({ text: text, ...params })
 }
 
+const validEmailRegex = RegExp(
+    /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i
+);
+
 function validation(obj) {
-  let inputs = obj.getElementsByTagName('input')
+    let inputs = obj.getElementsByTagName('input')
 
-  if (!(/[a-zA-Zа-яА-ЯёЁ]{3,25}/).test(inputs.name.value)) {
-   notify('Name is incorect!', 'warning')
-    return null
-  }
+    if (!inputs.name.value) {
+        notify('Name is required!', 'warning')
+        return null
+    }
 
-  if (!(/^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,10}/).test(inputs.email.value)) {
-    notify('Email is incorect!', 'warning')
-    return null
-  }
+    if (!inputs.email.value) {
+        notify('Email is required!', 'warning')
+        return null
+    }
 
-  if (!(/^[а-яА-ЯёЁa-zA-Z0-9]{3,255}/).test(inputs.message.value)) {
-    notify('Message is incorect!', 'warning')
-    return null
-  }
+    if (!validEmailRegex.test(inputs.email.value)) {
+        notify('Email is incorrect!', 'warning')
+        return null
+    }
 
-  return obj
+    if (!inputs.message.value) {
+        notify('Message is required!', 'warning')
+        return null
+    }
+
+    if (inputs.message.value.length < 10) {
+        console.log(inputs.message.value.length)
+        notify('Message must be 10 characters long!', 'warning')
+        return null
+    }
+
+    if (inputs.message.value.length > 255) {
+        console.log(inputs.message.value.length)
+        notify('Message is too long!', 'warning')
+        return null
+    }
+
+    return obj
 }
